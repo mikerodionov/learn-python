bb885052989ae6e2abc11be116f72533dbc641a1
Mikhail <rodionovmv@gmail.com>
2019-02-04 17:08:49 +0100

    commit

---
+++ conjuntos.py
@@ -0,0 +1,12 @@
+# conjunto = elementos agrupados (set)
+
+my_set = set('8923')
+print(my_set)
+
+my_set2 = {'8', '1', '5'}
+print(my_set2)
+
+# intersection
+print(my_set & my_set2)
+
+print(my_set.intersection(my_set2))
---
+++ ordenamiento.py
@@ -0,0 +1,12 @@
+# define list
+my_list = [9, 3, 5, 2, 7, 9, 0, 4]
+
+print(sorted(my_list))
+print(sorted(my_list, reverse=True))
+my_list.sort()
+print(my_list)
+
+my_list = ['Za', 'Fg', 'Az', 'Ij']
+print(sorted(my_list))
+my_list.sort()
+print(my_list)
